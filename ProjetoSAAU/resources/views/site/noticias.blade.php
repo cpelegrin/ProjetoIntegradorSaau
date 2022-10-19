82326944511add4063e21bee60a769474a92d809
RAFAEL HIDEKI MIYATAKE <rafael.9974@aluno.pr.senac.br>
2022-10-19 16:18:05 -0300

    instalação do summernote plugin

--- ProjetoSAAU/resources/views/site/noticias.blade.php
+++ ProjetoSAAU/resources/views/site/noticias.blade.php
@@ -3,13 +3,18 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
+
 <h1 class="m-0 text-dark">
     Notícias - Cadastro
 </h1>
 
 @stop
 
+@section('js')
 <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.1/dist/iconify-icon.min.js"></script>
+@stop
+
+@section('plugins.Summernote', true)
 
 @section('content')
 <div class="row">
@@ -38,7 +43,6 @@
                 <textarea class="form-control" placeholder="Resumo" rows="2"></textarea>
             </div>
 
-
             <!-- Img -->
             <div class="input-group">
                 <div class="input-group-prepend">
@@ -47,12 +51,37 @@
                     </span>
                 </div>
                 <div class="custom-file">
-                    <input type="file" class="custom-file-input" />
-                    <label class="custom-file-label">Selecione a imagem</label>
+                    <input type="file" class="custom-file-input" name="imagem" id="img" />
+                    <label class="custom-file-label" for="img">Selecione a imagem</label>
                 </div>
             </div>
-            <!--  -->
 
+            <div class="input-group mt-4">
+                <div class="input-group-prepend">
+                    <!-- teste do summernote -->
+                    @php
+                    $config = [
+                    'height' => '100',
+                    'toolbar' => [
+                    // [groupName, [list of button]]
+                    ['style', ['bold', 'italic', 'underline', 'clear']],
+                    ['font', ['strikethrough']],
+                    ['fontsize', ['fontsize']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['height', ['height']],
+                    ['table', ['table']],
+                    ['insert', ['link', 'picture']],
+                    ['view', ['fullscreen', 'codeview']],
+                    ],
+                    ];
+                    @endphp
+
+
+                    <x-adminlte-text-editor name="corpo_noticia" label="Corpo da Notícia" igroup-size="sm" placeholder="Digite e edite o corpo da notícia..." :config="$config" />
+
+                </div>
+            </div>
 
             <div class="mt-2">
                 <input type="submit" value="Enviar" class="btn btn-info" />
@@ -65,6 +94,7 @@
         </form>
 
     </div>
+
 </div>
 </div>
 </div>
