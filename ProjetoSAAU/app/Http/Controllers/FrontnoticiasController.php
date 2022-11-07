1281d89ec8b99b009f093265b4b924375beeaafc
JONATHAN ALLAN PEREIRA LARSSEN <jonathan.6908@aluno.pr.senac.br>
2022-11-07 17:08:03 -0300

    incluind alterações no front do blog

--- ProjetoSAAU/app/Http/Controllers/FrontnoticiasController.php
+++ ProjetoSAAU/app/Http/Controllers/FrontnoticiasController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Noticias;
 
 class FrontnoticiasController extends Controller
 {
     public function index()
     {
         $front = Noticias::get();
-        return view('site/blog');
+        dd($front);
+        return view('site.blog', compact('front'));
     }
 }
--- ProjetoSAAU/resources/views/site/blog.blade.php
+++ ProjetoSAAU/resources/views/site/blog.blade.php
@@ -1,4 +1,3 @@
-
 <body>
     <!--Titulo da pagina--->
     @section('title','SAAU - Notícias')
@@ -6,182 +5,180 @@
 
     <!--Inclusão do link inicio no menu--->
     @section('inicio')
-        <li><a href="{{Route('inicio')}}">INICIO</a></li>
+    <li><a href="{{Route('inicio')}}">INICIO</a></li>
     @endsection
     <!-- ============== -->
 
     <!--Inclusão da classe ativo no link do menu--->
     @section('ativo-blog','active')
     <!-- ============== -->
-    
+
     @extends('layouts.basico')
     @section('conteudo')
-        <main id="main">
-            <!-- ======= Breadcrumbs ======= -->
-            <section id="breadcrumbs" class="breadcrumbs">
-                <div class="container">
+    <main id="main">
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
 
-                    <ol>
-                        <li><a href="index.html">Início</a></li>
-                        <li>Blog</li>
-                    </ol>
-                    <h2>Notícias</h2>
+                <ol>
+                    <li><a href="index.html">Início</a></li>
+                    <li>Blog</li>
+                </ol>
+                <h2>Notícias</h2>
 
-                </div>
-            </section><!-- Final Breadcrumbs -->
+            </div>
+        </section><!-- Final Breadcrumbs -->
 
-            <!-- ======= Sessão Blog ======= -->
-            <section id="blog" class="blog">
-                <div class="container" data-aos="fade-up">
+        <!-- ======= Sessão Blog ======= -->
+        <section id="blog" class="blog">
+            <div class="container" data-aos="fade-up">
 
-                    <div class="row">
+                <div class="row">
 
-                        <div class="col-lg-8 entries">
+                    <div class="col-lg-8 entries">
 
-                            <article class="entry">
+                        <article class="entry">
 
-                                <div class="entry-img">
-                                    <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
+                            <div class="entry-img">
+                                <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
+                            </div>
+                            @foreach ($front as $noticia)
+                            <h2 class="entry-title">
+                                <a href="artigo-blog.html">Título da Postagem 1</a>
+                            </h2>
+
+                            <div class="entry-content">
+                                <p>
+                                    Aqui fica um resumo da notícia.
+                                </p>
+                                <div class="read-more">
+                                    <a href="artigo-blog.html">Continue Lendo...</a>
                                 </div>
+                            </div>
+                            @endforeach
+                        </article><!-- Entrada Sessão Blog  -->
 
-                                <h2 class="entry-title">
-                                    <a href="artigo-blog.html">Título da Postagem 1</a>
-                                </h2>
+                        <article class="entry">
 
-                                <div class="entry-content">
-                                    <p>
-                                        Aqui fica um resumo da notícia.
-                                    </p>
-                                    <div class="read-more">
-                                        <a href="artigo-blog.html">Continue Lendo...</a>
-                                    </div>
-                                </div>
-
-                            </article><!-- Entrada Sessão Blog  -->
-
-                            <article class="entry">
-
-                                <div class="entry-img">
-                                    <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
-                                </div>
-
-                                <h2 class="entry-title">
-                                    <a href="artigo-blog.html">Título da Postagem 2</a>
-                                </h2>
-
-                                <div class="entry-content">
-                                    <p>
-                                        Aqui fica um resumo da notícia
-                                    </p>
-                                    <div class="read-more">
-                                        <a href="artigo-blog.html">Continue Lendo...</a>
-                                    </div>
-                                </div>
-
-                            </article><!-- Entrada Sessão Blog -->
-
-                            <article class="entry">
-
-                                <div class="entry-img">
-                                    <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
-                                </div>
-
-                                <h2 class="entry-title">
-                                    <a href="artigo-blog.html">Título da Postagem 3</a>
-                                </h2>
-
-                                <div class="entry-content">
-                                    <p>
-                                        Aqui fica um resumo da notícia
-                                    </p>
-                                    <div class="read-more">
-                                        <a href="artigo-blog.html">Continue Lendo...</a>
-                                    </div>
-                                </div>
-
-                            </article><!-- Entrada Sessão Blog  -->
-
-                            <article class="entry">
-
-                                <div class="entry-img">
-                                    <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
-                                </div>
-
-                                <h2 class="entry-title">
-                                    <a href="artigo-blog.html">Título da Postagem 4</a>
-                                </h2>
-
-                                <div class="entry-content">
-                                    <p>
-                                        Aqui fica um resumo da notícia
-                                    </p>
-                                    <div class="read-more">
-                                        <a href="artigo-blog.html">Continue Lendo...</a>
-                                    </div>
-                                </div>
-
-                            </article><!-- Entrada Sessão Blog  -->
-
-                            <div class="blog-pagination">
-                                <ul class="justify-content-center">
-                                    <li class="active"><a href="#">1</a></li>
-                                    <li><a href="#">2</a></li>
-                                    <li><a href="#">3</a></li>
-                                </ul>
+                            <div class="entry-img">
+                                <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
                             </div>
 
-                        </div><!-- Final lista entrada blog -->
+                            <h2 class="entry-title">
+                                <a href="artigo-blog.html">Título da Postagem 2</a>
+                            </h2>
 
-                        <div class="col-lg-4">
+                            <div class="entry-content">
+                                <p>
+                                    Aqui fica um resumo da notícia
+                                </p>
+                                <div class="read-more">
+                                    <a href="artigo-blog.html">Continue Lendo...</a>
+                                </div>
+                            </div>
 
-                            <div class="sidebar">
+                        </article><!-- Entrada Sessão Blog -->
 
-                                <h3 class="sidebar-title">Procurar:</h3>
-                                <div class="sidebar-item search-form">
-                                    <form action="">
-                                        <input type="text">
-                                        <button type="submit"><i class="bi bi-search"></i></button>
-                                    </form>
-                                </div><!-- Final formulário de pesquisa-->
+                        <article class="entry">
 
-                                <h3 class="sidebar-title">Categorias:</h3>
-                                <div class="sidebar-item categories">
-                                    <ul>
-                                        <li><a href="#">Gatos <span>(1)</span></a></li>
-                                        <li><a href="#">Cachorros <span>(4)</span></a></li>
-                                        <li><a href="#">Estilo de Vida <span>(2)</span></a></li>
-                                        <li><a href="#">Educação <span>(6)</span></a></li>
-                                        <li><a href="#">Cuidados <span>(3)</span></a></li>
-                                    </ul>
-                                </div><!-- Final sidebar categorias-->
+                            <div class="entry-img">
+                                <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
+                            </div>
 
-                                <h3 class="sidebar-title">Postagens Recentes:</h3>
-                                <div class="sidebar-item recent-posts">
-                                    <div class="post-item clearfix">
-                                        <img src="assets/img/blog/mordida.jpg" alt="">
-                                        <h4><a href="blog-single.html">Título da Postagem</a></h4>
-                                    </div>
+                            <h2 class="entry-title">
+                                <a href="artigo-blog.html">Título da Postagem 3</a>
+                            </h2>
 
-                                    <div class="post-item clearfix">
-                                        <img src="assets/img/blog/mordida.jpg" alt="">
-                                        <h4><a href="blog-single.html">Título da Postagem</a></h4>
-                                    </div>
+                            <div class="entry-content">
+                                <p>
+                                    Aqui fica um resumo da notícia
+                                </p>
+                                <div class="read-more">
+                                    <a href="artigo-blog.html">Continue Lendo...</a>
+                                </div>
+                            </div>
 
-                                    <div class="post-item clearfix">
-                                        <img src="assets/img/blog/mordida.jpg" alt="">
-                                        <h4><a href="blog-single.html">Título da Postagem</a></h4>
-                                    </div>
+                        </article><!-- Entrada Sessão Blog  -->
 
-                                    <div class="post-item clearfix">
-                                        <img src="assets/img/blog/mordida.jpg" alt="">
-                                        <h4><a href="blog-single.html">Título da Postagem</a></h4>
-                                    </div>
+                        <article class="entry">
 
-                                    <div class="post-item clearfix">
-                                        <img src="assets/img/blog/mordida.jpg" alt="">
-                                        <h4><a href="blog-single.html">Título da Postagem</a></h4>
-                                    </div>
+                            <div class="entry-img">
+                                <img src="assets/img/blog/mordida.jpg" alt="" class="img-fluid">
+                            </div>
 
+                            <h2 class="entry-title">
+                                <a href="artigo-blog.html">Título da Postagem 4</a>
+                            </h2>
+
+                            <div class="entry-content">
+                                <p>
+                                    Aqui fica um resumo da notícia
+                                </p>
+                                <div class="read-more">
+                                    <a href="artigo-blog.html">Continue Lendo...</a>
+                                </div>
+                            </div>
+
+                        </article><!-- Entrada Sessão Blog  -->
+
+                        <div class="blog-pagination">
+                            <ul class="justify-content-center">
+                                <li class="active"><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                            </ul>
+                        </div>
+
+                    </div><!-- Final lista entrada blog -->
+
+                    <div class="col-lg-4">
+
+                        <div class="sidebar">
+
+                            <h3 class="sidebar-title">Procurar:</h3>
+                            <div class="sidebar-item search-form">
+                                <form action="">
+                                    <input type="text">
+                                    <button type="submit"><i class="bi bi-search"></i></button>
+                                </form>
+                            </div><!-- Final formulário de pesquisa-->
+
+                            <h3 class="sidebar-title">Categorias:</h3>
+                            <div class="sidebar-item categories">
+                                <ul>
+                                    <li><a href="#">Gatos <span>(1)</span></a></li>
+                                    <li><a href="#">Cachorros <span>(4)</span></a></li>
+                                    <li><a href="#">Estilo de Vida <span>(2)</span></a></li>
+                                    <li><a href="#">Educação <span>(6)</span></a></li>
+                                    <li><a href="#">Cuidados <span>(3)</span></a></li>
+                                </ul>
+                            </div><!-- Final sidebar categorias-->
+
+                            <h3 class="sidebar-title">Postagens Recentes:</h3>
+                            <div class="sidebar-item recent-posts">
+                                <div class="post-item clearfix">
+                                    <img src="assets/img/blog/mordida.jpg" alt="">
+                                    <h4><a href="blog-single.html">Título da Postagem</a></h4>
+                                </div>
+
+                                <div class="post-item clearfix">
+                                    <img src="assets/img/blog/mordida.jpg" alt="">
+                                    <h4><a href="blog-single.html">Título da Postagem</a></h4>
+                                </div>
+
+                                <div class="post-item clearfix">
+                                    <img src="assets/img/blog/mordida.jpg" alt="">
+                                    <h4><a href="blog-single.html">Título da Postagem</a></h4>
+                                </div>
+
+                                <div class="post-item clearfix">
+                                    <img src="assets/img/blog/mordida.jpg" alt="">
+                                    <h4><a href="blog-single.html">Título da Postagem</a></h4>
+                                </div>
+
+                                <div class="post-item clearfix">
+                                    <img src="assets/img/blog/mordida.jpg" alt="">
+                                    <h4><a href="blog-single.html">Título da Postagem</a></h4>
                                 </div>
 
                             </div>
@@ -191,11 +188,13 @@
                     </div>
 
                 </div>
-            </section>
 
-        </main>
+            </div>
+        </section>
+
+    </main>
     @endsection('conteudo')
-    
+
 </body>
 
 </html>
