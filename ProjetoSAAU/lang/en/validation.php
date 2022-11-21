ee99b4691b137ad0831bcdd88bbcc12bcbd98bac
RAFAEL HIDEKI MIYATAKE <rafael.9974@aluno.pr.senac.br>
2022-11-21 15:01:33 -0300

    Ajuste de timezone e formatação de data com o carbon

--- ProjetoSAAU/lang/en/validation.php
+++ ProjetoSAAU/lang/en/validation.php
@@ -134,7 +134,7 @@ return [
     ],
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
-    'timezone' => 'The :attribute must be a valid timezone.',
+    'timezone' => 'The :attribute must be a valid .',
     'unique' => 'The :attribute has already been taken.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
--- ProjetoSAAU/resources/views/animal/mostrarAnimal.blade.php
+++ ProjetoSAAU/resources/views/animal/mostrarAnimal.blade.php
@@ -6,15 +6,86 @@
 @stop
 
 @section('content')
+
+
+@php
+$heads = [
+'Nome',
+'Sexo',
+
+['label' => 'Ações', 'no-export' => true, 'width' => 15]
+];
+
+$data = [];
+foreach($animal as $animals){
+
+$btnEdit = '<a href="'. route('noticias.edit',['id'=>$noticia->id]).'" class=" mx-2"><i class="fas fa-user-edit text-info" aria-hidden="true"></i></a>';
+$btnDelete='<a href="#" class="mx-2 deletebutton" data-toggle="modal" data-target="#deletarnoticia" data-noticiaid="'.$noticia->id.'" data-noticiatitle="'.$noticia->titulo.'"><i class="fas fa-trash text-danger" aria-hidden="true"></i></a>';
+array_push($data, array($noticia->titulo, $noticia->resumo,$newDate,'<nobr>'.$btnEdit.$btnDelete.'</nobr>'
+)
+);
+}
+$config = ['data' => $data,
+'order' => [[1, 'asc']],
+'columns' => [null, ['orderable' => true], null],
+];
+
+@endphp
+{{-- Minimal example / fill data using the component slot --}}
+<x-adminlte-datatable id="table1" :heads="$heads">
+    @foreach($config['data'] as $row)
+    <tr>
+        @foreach($row as $cell)
+        <td>{!! $cell !!}</td>
+        @endforeach
+    </tr>
+    @endforeach
+</x-adminlte-datatable>
+<!-- Modal -->
+<form id="deleteForm" method="post" action="{{route('remover.noticias', 1)}}">
+    @csrf
+    @method('put')
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div class="modal fade" id="deletarnoticia" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Confirmação</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p class="text-center">Confirma a exclusão do registro?</p>
+                    <p class="text-center" id=titulo_noticia></p>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-danger"> Deletar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+
+@stop
+
+
+
 <div class="row ">
     <div class="col-10 ">
         <div class=" ">
             <div class="card-body">
-                <div class="col-md-10" >
+                <div class="col-md-10">
                     <div class="card">
                         <!--Titulo da tabela-->
                         <div class="card-header card-header-icon" data-background-color="rose">
-                            <i class="material-icons"><h4>Animais cadastrados </h4></i>
+                            <i class="material-icons">
+                                <h4>Animais cadastrados </h4>
+                            </i>
                         </div>
                         <!--===============-->
                         <div class="card-content pl-2">
@@ -61,3 +132,42 @@
     </div>
 </div>
 @stop
+
+@section('js')
+<script>
+    $('#table1').ready(function() {
+        if ($.fn.dataTable.isDataTable('#table1')) {
+            table = $('#table1').DataTable();
+            //TODO trocar a linguagem 
+        } else {
+            $('#table1').DataTable({
+                'language': {
+                    'url': '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json'
+                },
+
+            });
+        }
+        table.destroy();
+
+        table = $('#table1').DataTable({
+            'language': {
+                'url': '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json'
+            },
+
+        });
+    });
+</script>
+
+<script>
+    $('.deletebutton').on('click', function() {
+        var noticiaid = this.dataset['noticiaid'];
+        var noticia_titulo = this.dataset['noticiatitle'];
+        console.log(noticia_titulo);
+        $('#titulo_noticia').html("<b>" + noticia_titulo + "</b>");
+        var gambi = $('#deleteForm').attr('action');
+        var s = gambi.substr(gambi.indexOf('remover/') + 8);
+        var final = gambi.replace(s, noticiaid)
+        $('#deleteForm').attr('action', final);
+    });
+</script>
+@endsection
